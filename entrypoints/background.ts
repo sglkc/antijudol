59d3fa9c85d0ec328c0d8491b29ee32b711b9fd9
Seya <31957516+sglkc@users.noreply.github.com>
2025-02-15 15:33:19 +0700

    feat(background): refresh offscreen jika sudah ada

--- entrypoints/background.ts
+++ entrypoints/background.ts
@@ -1,11 +1,17 @@
-export default defineBackground(() => {
-  console.log('Hello background!', { id: browser.runtime.id });
+async function initOffscreen() {
+  if (await chrome.offscreen.hasDocument()) {
+    await chrome.offscreen.closeDocument()
+  }
 
   chrome.offscreen.createDocument({
     url: '/offscreen.html',
     reasons: [chrome.offscreen.Reason.BLOBS],
     justification: 'URL not found'
   })
+}
 
+export default defineBackground(() => {
+  console.log('Hello background!', { id: browser.runtime.id });
+  initOffscreen()
   chrome.runtime.onMessage.addListener(console.log)
 });
